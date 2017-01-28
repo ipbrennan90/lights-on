e59a9073e09e82167d423ebe842d8f78612496d4
ipbrennan <ian.brennan@icloud.com>
2017-01-28 06:19:58 -0700

    lights turning on and off

--- src/components/Light/Light.jsx
+++ src/components/Light/Light.jsx
@@ -1,7 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 
+const styles = require('./Light.scss')
 export default class Light extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state  = {
+			light: 'off'
+		};
+		this.switchLight = this.switchLight.bind(this);
+	}
+
+	switchLight() {
+		this.state.light === 'off' ? this.setState({light: 'on'}) : this.setState({light: 'off'});
+	}
+
+	getLightStyle() {
+		return this.state.light === 'on' ? "light on" : "light"
+	}
+
 	render() {
-		return <button></button>
+		return <button className={this.getLightStyle()} onClick={this.switchLight}></button>
 	}
 }
--- test/components/Light_spec.jsx
+++ test/components/Light_spec.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
 	renderIntoDocument,
-	scryRenderedDOMComponentsWithTag
+	scryRenderedDOMComponentsWithTag,
+	Simulate
 } from 'react-addons-test-utils';
 import { expect } from 'chai';
 import Light from '../../src/components/Light/Light';
@@ -14,5 +15,17 @@ describe('Light', () => {
 		)
 		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 		expect(buttons.length).to.equal(1);
+	})
+
+	it('turns on and off', () => {
+		const component = renderIntoDocument(
+			<Light/>
+		)
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		expect(component.state.light).to.equal('off')
+		Simulate.click(buttons[0])
+		expect(component.state.light).to.equal('on')
+		Simulate.click(buttons[0])
+		expect(component.state.light).to.equal('off')
 	})
 })
